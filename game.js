801dbfbe62e24f61078bf37c27e7b8e0f024b9c4
Travis Beatty <travisby@gmail.com>
2014-01-22 13:57:22 -0500

    Refactored game.js to use localvars

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var utils = require('./utils.js');
 
-module.exports.game = function(socketio) {
+var game = function(socketio) {
     socketio.on(
         'join',
         function (room) {
@@ -11,18 +11,18 @@ module.exports.game = function(socketio) {
 };
 
 
-module.exports.Card = function(rank, suit) {
+var Card = function(rank, suit) {
     // make sure rank is correct
-    if (!(2 <= rank <= module.exports.Card.ACE)) {
+    if (!(2 <= rank <= Card.ACE)) {
         console.log(rank);
         throw "Incorrect rank"
     }
     // make sure the suit is correct
     switch (suit) {
-        case module.exports.Card.DIAMONDS:
-        case module.exports.Card.CLUBS:
-        case module.exports.Card.SPADES:
-        case module.exports.Card.HEARTS:
+        case Card.DIAMONDS:
+        case Card.CLUBS:
+        case Card.SPADES:
+        case Card.HEARTS:
             // everything is fine!
             break;
         default:
@@ -33,54 +33,54 @@ module.exports.Card = function(rank, suit) {
     this.rank = rank;
     this.suit = suit;
 };
-module.exports.Card.prototype.suit = module.exports.Card.DIAMONDS;
-module.exports.Card.prototype.rank = 2;
-module.exports.Card.prototype.toString = function() {
+Card.prototype.suit = Card.DIAMONDS;
+Card.prototype.rank = 2;
+Card.prototype.toString = function() {
     return [this.rank, this.suit].join('');
 };
-module.exports.Card.prototype.toJSON = module.exports.Card.prototype.toString;
-module.exports.Card.DIAMONDS = 'd';
-module.exports.Card.CLUBS = 'c';
-module.exports.Card.SPADES = 's';
-module.exports.Card.HEARTS = 'h';
-module.exports.Card.JACK = 11
-module.exports.Card.QUEEN = 12;
-module.exports.Card.KING = 13;
-module.exports.Card.ACE = 14;
-module.exports.Card.RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, module.exports.Card.JACK, module.exports.Card.QUEEN, module.exports.Card.KING, module.exports.Card.ACE];
-module.exports.Card.SUITS = [module.exports.Card.HEARTS, module.exports.Card.SPADES, module.exports.Card.CLUBS, module.exports.Card.DIAMONDS];
-module.exports.Card.isActuallyACard = function (card) {
+Card.prototype.toJSON = Card.prototype.toString;
+Card.DIAMONDS = 'd';
+Card.CLUBS = 'c';
+Card.SPADES = 's';
+Card.HEARTS = 'h';
+Card.JACK = 11
+Card.QUEEN = 12;
+Card.KING = 13;
+Card.ACE = 14;
+Card.RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, Card.JACK, Card.QUEEN, Card.KING, Card.ACE];
+Card.SUITS = [Card.HEARTS, Card.SPADES, Card.CLUBS, Card.DIAMONDS];
+Card.isActuallyACard = function (card) {
     return card.hasOwnProperty('rank') && card.hasOwnProperty('suit');
 };
 
-module.exports.Hand = function(cards) {
+var Hand = function(cards) {
     // Check that we are in fact, cards
-    if (!utils.all(module.exports.Card.isActuallyACard, cards)) {
+    if (!utils.all(Card.isActuallyACard, cards)) {
         throw "Not all items in hand are cards";
     }
     this.cards = cards;
 };
-module.exports.Hand.prototype.cards = [];
+Hand.prototype.cards = [];
 
-module.exports.Deck = function(cards) {
+var Deck = function(cards) {
     // Check that we are in fact, cards
-    if (!utils.all(module.exports.Card.isActuallyACard, cards)) {
+    if (!utils.all(Card.isActuallyACard, cards)) {
         throw "Not all items in hand are cards";
     }
     this.cards = cards;
 };
-module.exports.Deck.prototype.cards = [];
-module.exports.Deck.prototype.toJSON = function () {
+Deck.prototype.cards = [];
+Deck.prototype.toJSON = function () {
     return this.cards.map(
         function (card) {
             return card.toJSON();
         }
     );
 };
-module.exports.Deck.prototype.toString = function () {
+Deck.prototype.toString = function () {
     return this.cards.join(',');
 }
-module.exports.Deck.prototype.shuffle = function () {
+Deck.prototype.shuffle = function () {
     // Fischer-Yates Shuffle
     for (var i = this.cards.length -1; i--; i <= 1) {
         var j = Math.round(Math.random() * i);
@@ -90,21 +90,27 @@ module.exports.Deck.prototype.shuffle = function () {
     }
 }
 
-module.exports.PokerDeck = function() {
+var PokerDeck = function() {
     // have a place to store all 52 cards
     var cards = [];
 
     // Create all 52 cards, and fill cards
-    module.exports.Card.SUITS.forEach(
+    Card.SUITS.forEach(
         function (suit) {
-            module.exports.Card.RANKS.forEach(
+            Card.RANKS.forEach(
                 function (rank) {
-                    cards.push(new module.exports.Card(rank, suit));
+                    cards.push(new Card(rank, suit));
                 }
             );
         }
     );
 
     // build a deck with these cards, and use it as the return value
-    return new module.exports.Deck(cards);
-};
+    return new Deck(cards);
+}
+
+module.exports.game = game;
+module.exports.Card = Card;
+module.exports.Hand = Hand;
+module.exports.Deck = Deck;
+module.exports.PokerDeck = PokerDeck;
