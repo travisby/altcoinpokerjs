45ad572c818400a6cd0e1a5df5f037116d3e46e0
Travis Beatty <travisby@gmail.com>
2014-01-31 09:18:33 -0500

    Added events to public js

--- public/javascripts/poker.js
+++ public/javascripts/poker.js
@@ -1,3 +1,63 @@
+/**
+ * Betting Object Type Decleration
+ *
+ * @typedef {Object} betObj
+ * @property {number} amount - number of coins to bet
+ * @property {Player} nextPlayer - the player that bet
+ */
+
+// events
+/**
+ * Event for users connecting to the socketio socket
+ *
+ * @event connection
+ */
+var connection = 'connection';
+
+/**
+ * Event for users joining a room
+ *
+ * @event join
+ * @type {number} -  the room ID to join
+ */
+var join = 'join';
+
+/**
+ * Event for sending hole cards to players
+ *
+ * @event newPlayerCards
+ * @type {object}
+ * @property {string} cards - ',' delimited list of cards
+ */
+var newPlayerCards = 'newPlayerCards';
+
+/**
+ * Event to broadcast that the hand has been dealt
+ *
+ * @event deal
+ */
+var deal = 'deal';
+
+/**
+ * Event to be sent to inform everyone of a bet
+ *
+ * @event bet
+ */
+var bet = 'bet';
+
+/**
+ * Event to tell the server that a specific user is ready
+ *
+ * @event ready
+ */
+var ready = 'ready';
+
+/**
+ * Event when a user leaves the game, whether accidentally or purposely
+ *
+ * @event disconnect
+ */
+var disconnect = 'disconnect';
 var socket = io.connect('/pokersocket');
 socket.on('connect', function (data) {
     console.log("asking to join room " + roomID);
