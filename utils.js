d3186b093d5668f0ec24f96193e29ae0e3a08d1f
Travis Beatty <travisby@gmail.com>
2014-02-04 15:54:28 -0500

    Added an any() function to our utilities

--- utils.js
+++ utils.js
@@ -8,4 +8,16 @@ module.exports.all = function (callback, arr) {
             }
         }
     );
-}
+};
+
+module.exports.any = function (callback, arr) {
+    return arr.map(callback).reduce(
+        function (previousValue, currentValue, index, array) {
+            if (previousValue) {
+                return previousValue;
+            } else {
+                return currentValue;
+            }
+        }
+    );
+};
