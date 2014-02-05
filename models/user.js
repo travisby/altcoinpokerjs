225abf334fe22fafe574c239e394d6c881187ec7
Travis Beatty <travisby@gmail.com>
2014-02-05 17:52:35 -0500

    Added passport-local-mongoose

--- models/user.js
+++ models/user.js
@@ -1,11 +1,17 @@
 module.exports = function (mongoose) {
+    var passportLocalMongoose = require('passport-local-mongoose');
     var collection = 'users';
 
     var schema = new mongoose.Schema(
         {
-            username: String,
             players: [{type: mongoose.Schema.ObjectId, ref: 'players'}],
             payouts: [{type: mongoose.Schema.ObjectId, ref: 'payouts'}]
+        }
+    );
+
+    schema.plugin(
+        passportLocalMongoose,
+        {
         }
     );
 
