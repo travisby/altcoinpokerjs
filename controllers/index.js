10e067f768fe5bbf9cd2435649383e51d73ab591
Travis Beatty <travisby@gmail.com>
2014-02-07 08:55:38 -0500

    Added wallet stuff to user settings

--- controllers/index.js
+++ controllers/index.js
@@ -4,30 +4,12 @@ module.exports.controller = function(app) {
     app.get(
         '/',
         function(req, res) {
-            var obj = {
-                title: 'Welcome to CryptoPoker!',
-            };
-            if (req.user) {
-                obj.user = req.user;
-                db.Payout.where('user', req.user.id).populate('currency').exec(
-                    req.user._id,
-                    function (err, payouts) {
-                        if (err) {
-                            throw err;
-                        }
-                        obj.payouts = payouts;
-                        res.render(
-                            'index',
-                            obj
-                        );
-                    }
-                );
-            } else {
-                res.render(
-                    'index',
-                    obj
-                );
-            }
+            res.render(
+                'index',
+                {
+                    title: 'Welcome to CryptoPoker!'
+                }
+            );
         }
     );
 };
