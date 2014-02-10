d66747a42822f5170378914ca426a52b205332ba
Travis Beatty <travisby@gmail.com>
2014-02-10 12:15:43 -0500

    moved some stuff around

--- public/javascripts/poker.js
+++ public/javascripts/poker.js
@@ -6,6 +6,63 @@
  * @property {Player} nextPlayer - the player that bet
  */
 
+var stringToCardCharacter = function (cardString) {
+    // this is the value of the back of a card
+    var decimalValue = 127136;
+    var rankCharacter = cardString.charAt(0);
+    var suitCharacter = cardString.charAt(1);
+
+    // must be trying to get the back of a card!
+    if (cardString === '') {
+        return '&#' + decimalValue;
+    }
+
+    switch (suitCharacter) {
+        case 'c':
+            decimalValue += 48;
+            break;
+        case 'd':
+            decimalValue += 32;
+            break;
+        case 'h':
+            decimalValue += 16;
+            break;
+        case 's':
+            decimalValue += 0;
+            break;
+        default:
+            decimalValue += 0;
+            break;
+    }
+
+    if (rankCharacter === 'A') {
+        decimalValue += 1;
+    } else if (rankCharacter === 'K') {
+        decimalValue += 14;
+    } else if (rankCharacter === 'Q') {
+        decimalValue += 13;
+    } else if (rankCharacter === 'J') {
+        decimalValue += 12;
+    } else if (rankCharacter === 'T') {
+        decimalValue += 10;
+    } else {
+        // in this case, we must be numeric
+        decimalValue += parseInt(rankCharacter);
+    }
+
+    return '&#' + decimalValue;
+
+};
+
+// graphic objects
+var pokerTable = PIXI.Sprite.fromImage('/vendor/imgs/poker_table.png'); 
+var deck = new PIXI.Text($('<div />').html(stringToCardCharacter('')).text());
+var readyButton = new PIXI.Text('Ready');
+var checkOrFold = new PIXI.Text('Check/Fold');
+var betOrRaise = new PIXI.Text('Bet/Raise');
+var players = [];
+var cards = [];
+
 // events
 /**
  * Event for users connecting to the socketio socket
@@ -125,94 +182,46 @@ socket.on(
 );
 
 /**
- * Listens for deal, letting us know cards are being dealt
+ * Card object
  *
- * We should draw to the screen
- *
- * @listens deal
+ * @param {String} str - the string making a particular card up
  */
-socket.on(
-    deal,
-    function () {
-        console.log("Listened to a deal event");
-        // TODO draw cards being dealt
-    }
-);
+var Card = function (str) {
+    this.sprite = new PIXI.Text($('<div />').html(stringToCardCharacter(str)).text());
+};
 
 /**
- * Let the server know we are placing a bet
+ * Contains the PIXI sprite for us to use
  *
- * @fires bet
+ * @type PIXI.Sprite
  */
-$('#betButton')[0].onclick = function () {
-    console.log("Firing bet event");
-    socket.emit(
-        bet,
-        {
-            amount: parseInt($('#betAmountField')[0].value),
-            // not used on this end... TODO refactor and use a separate object
-            nextPlayer: null
-        }
+Card.prototype.sprite = null;
+
+/**
+ * Animate this card to position point over time frames
+ *
+ * @param {PIXI.Point} point - the point we are moving to
+ * @param {Number} frames - how many frames should it take to get there?
+ */
+Card.prototype.animateTo = function (point, frames) {
+    var amount = new PIXI.Point();
+    amount.x = (point.x - this.sprite.position.x) / frames;
+    amount.y = (point.y - this.sprite.position.y) / frames;
+
+    animations.push(
+        function (amount, times) {
+            if (times < 0) {
+                return;
+            }
+            this.position.x -= point.x;
+            this.position.y -= point.y;
+            animations.push(this, [amount, times - 1]);
+        },
+        [amount, frames]
     );
 };
 
-/**
- * Player readys-up
- *
- * @fires ready
- */
-$('#readyButton')[0].onclick = function () {
-    console.log("Firing ready event");
-    socket.emit(ready);
-};
 
-var stringToCardCharacter = function (cardString) {
-    // this is the value of the back of a card
-    var decimalValue = 127136;
-    var rankCharacter = cardString.charAt(0);
-    var suitCharacter = cardString.charAt(1);
-
-    // must be trying to get the back of a card!
-    if (cardString === '') {
-        return '&#' + decimalValue;
-    }
-
-    switch (suitCharacter) {
-        case 'c':
-            decimalValue += 48;
-            break;
-        case 'd':
-            decimalValue += 32;
-            break;
-        case 'h':
-            decimalValue += 16;
-            break;
-        case 's':
-            decimalValue += 0;
-            break;
-        default:
-            decimalValue += 0;
-            break;
-    }
-
-    if (rankCharacter === 'A') {
-        decimalValue += 1;
-    } else if (rankCharacter === 'K') {
-        decimalValue += 14;
-    } else if (rankCharacter === 'Q') {
-        decimalValue += 13;
-    } else if (rankCharacter === 'J') {
-        decimalValue += 12;
-    } else if (rankCharacter === 'T') {
-        decimalValue += 10;
-    } else {
-        // in this case, we must be numeric
-        decimalValue += parseInt(rankCharacter);
-    }
-
-    return '&#' + decimalValue;
-
-};
 
 var toSpan = function (string, klass) {
     return "<span class='" + klass + "'>" + string + '</span>';
@@ -227,6 +236,13 @@ $(document).ready(
         var $canvas = $('canvas');
         // create the root of the interactive scenegraph
         var stage = new PIXI.Stage(BLACK, true);
+        stage.addChild(pokerTable);
+        console.log(readyButton);
+        stage.addChild(readyButton);
+        stage.addChild(checkOrFold);
+        stage.addChild(betOrRaise);
+        stage.addChild(deck);
+
 
         // decide between canvas and webgl for us
         var renderer = PIXI.autoDetectRenderer(
@@ -235,8 +251,6 @@ $(document).ready(
             $canvas[0]
         );
 
-        var pokerTable = PIXI.Sprite.fromImage('/vendor/imgs/poker_table.png'); 
-        var deck = new PIXI.Text($('<div />').html(stringToCardCharacter('')).text());
         deck.scale = new PIXI.Point(3,3);
         deck.position.x = 700;
         deck.position.y = 450;
